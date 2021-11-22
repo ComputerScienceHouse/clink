656c1545e0fbdd81a7e9ff79d678450cd0c16826
Mary Strodl <ipadlover8322@gmail.com>
2021-11-22 17:26:16 -0500

    WIP drop

--- src/api.rs
+++ src/api.rs
@@ -1,6 +1,8 @@
 use std::fmt;
 use url::Url;
-use isahc::{Request, auth::Authentication, prelude::*};
+use isahc::{Request, auth::Authentication, prelude::*, HttpClient};
+use http::status::StatusCode;
+use serde_json::json;
 
 pub struct API {
   token: Option<String>,
@@ -28,6 +30,22 @@ impl API {
     return API {
       token: None
     };
+  }
+  pub fn drop(self: &mut API, machine: String, slot: u8) -> Result<(), Box<dyn std::error::Error>> {
+    let token = self.get_token()?;
+
+    let client = HttpClient::new()?;
+    let request = Request::post("https://drink.csh.rit.edu/drinks/drop")
+      .header("Authorization", token)
+      .body(json!({
+        "machine": machine,
+        "slot": slot,
+      }).to_string())?;
+    let response = client.send(request)?;
+    return match response.status() {
+      StatusCode::OK => Ok(()),
+      _ => Err(Box::new(APIError::BadFormat))
+    }
   }
   pub fn get_token(self: &mut API) -> Result<String, Box<dyn std::error::Error>> {
     return match &self.token {
